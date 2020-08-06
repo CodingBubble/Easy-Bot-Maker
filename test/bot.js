2b73031dd5ff70ea9a4f56b14060c85cf99446a4
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2020-08-06 08:56:29 +0200

    Erstellen Fertig

---
+++ test/bot.js
@@ -0,0 +1,396 @@
+
+const { TeamSpeak, QueryProtocol, TeamSpeakChannel  } = require("ts3-nodejs-library")
+
+const config = require("./config.json")
+const { Command } = require("ts3-nodejs-library/lib/transport/Command")
+
+
+
+
+const teamspeak = new TeamSpeak({
+  host: config.host,
+  queryport: config.port, 
+  serverport: 9987,
+  username: config.login,
+  password: config.password,
+  nickname: config.name
+})
+
+
+
+teamspeak.on("ready", async () => {
+  	console.log("Connected")
+	
+
+	Promise.all([
+    teamspeak.registerEvent("server"),
+    teamspeak.registerEvent("channel", 0),
+    teamspeak.registerEvent("textserver"),
+    teamspeak.registerEvent("textchannel"),
+    teamspeak.registerEvent("textprivate")
+	  ]).then(() => {
+		  console.log("Subscribed to all Events")
+	  }).catch(e => {
+		  console.log("CATCHED", e.message)
+	})
+
+
+	const clients = await teamspeak.clientList()
+	console.log("Sending Welcome Message to all Players")
+	clients.forEach(client => {
+		if (client.propcache.clientType==0)
+		{
+			
+			client.message("The Bot is now online!")
+		
+		}
+	})
+	
+	
+})
+
+
+
+
+teamspeak.on("error", () => {
+	console.log("An Error Occured")
+})
+
+
+
+
+
+teamspeak.on("textmessage", async(ev) => {
+	const sender = ev.invoker.propcache.clientNickname;
+	var msg = ev.msg;
+	if (msg.charAt(0)=='!')
+	{
+		msg = msg.substr(1)
+		var command = msg.split(" ")[0];
+		var args = []
+		try
+		{
+			 args = msg.split(" ").shift();
+		}
+		catch (e)
+		{
+			console.log(e)
+			return
+		}		
+		HandleCommand (command, args, sender)
+	}
+})
+
+
+
+config.events.forEach(Eve=>{
+	teamspeak.on(Eve.name.toLowerCase(), async(ev) => {
+		
+		var sender; 
+		try 
+		{
+			sender = ev.client.propcache.clientNickname
+		} catch 
+		{
+			sender = ev.invoker.propcache.clientNickname;
+		}
+		
+		var cond = ""
+		switch (Eve.name.toLowerCase())
+		{
+			case "textmessage":
+				cond = ev.msg;
+				break
+			case "clientmoved":
+				cond = ev.channel.name
+				
+		}
+
+
+
+		if (Eve.condition=="" || Eve.condition == cond)
+		{
+			Actionargs = [...Eve["action"]["args"]]
+			for (var i=0; i!=Actionargs.length; i++)
+			{
+				Actionargs[i] = Actionargs[i].replace(new RegExp("\\{s\\}","g"), sender);
+			}
+	
+	
+			ExecuteAction(Eve["action"]["name"], Actionargs)
+		}
+
+
+	})
+})
+
+
+
+
+
+
+
+function HandleCommand(Command, Args, Sender)
+{
+	config.commands.forEach(command => {	
+			if (command["name"] == Command)
+			{
+				Actionargs = [...command["action"]["args"]]
+				for (var i=0; i!=Actionargs.length; i++)
+				{
+					Actionargs[i] = Actionargs[i].replace(new RegExp("\\{s\\}","g"), Sender);
+					for (var j=0; j!=Args.length; j++)
+					{
+						Actionargs[i] = Actionargs[i].replace((new RegExp("\\{"+j+"\\}", "g")), Args[j]);
+					}
+				}
+
+		
+				ExecuteAction(command["action"]["name"], Actionargs)
+			}
+	})
+
+                
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function ExecuteAction (Action, Args)
+{
+
+	Action = Action.toLowerCase()
+	console.log(Action)
+	console.log(Args)	
+	var client;
+
+	switch (Action)
+	{
+
+
+		
+
+		case "sendtextmessage":	
+	
+			client = await teamspeak.getClientByName(Args[0]);
+		
+			client.message(Args[1])
+			break
+
+
+		case "sendmessagetogroup":	
+		
+			try
+			{
+				const clients = await teamspeak.clientList();
+				const group = Args[0];
+
+				clients.forEach(client => {
+					client.servergroups.forEach(async(GroupId)=>{
+						const Group = await teamspeak.getServerGroupById(GroupId);
+						GroupName = Group.name;				
+						if (client.propcache.clientType==0 && group==GroupName)
+						{		
+							client.message(Args[1])
+						}
+					})
+
+				})
+			}
+			catch 
+			{
+
+			}
+
+			break
+	
+			
+			case "sendmessagetoall":	
+		
+			try
+			{
+				client = await teamspeak.getClientByName(Args[0]);
+				const clients = await teamspeak.clientList();
+
+				clients.forEach(client => {		
+					if (client.propcache.clientType==0)
+					{		
+						client.message(Args[0])
+					}
+				})
+			}
+			catch 
+			{
+
+			}
+
+			break
+	
+
+
+		case "addservergroup":
+			try
+			{
+				client = await teamspeak.getClientByName(Args[0]);
+
+				const Groups = await teamspeak.serverGroupList()
+
+				Group = null
+
+				Groups.forEach(g => {
+					if (g.name==Args[1] && g.type==1)
+					{
+						Group = g
+					}
+				})
+
+				if (Group != null)
+				{
+					try 
+					{
+						Group.addClient(client).catch(()=>{})
+					}
+					catch{}
+				}
+			} 
+			catch
+			{
+
+			}
+			break
+
+	
+
+		case "removeservergroup":	
+			try
+			{
+				client = await teamspeak.getClientByName(Args[0]);
+				const Groups = await teamspeak.serverGroupList()
+				Group = null
+				Groups.forEach(g => {
+					if (g.name==Args[1] && g.type==1)
+					{
+						Group = g						
+					}
+
+				})
+
+				if (Group != null)
+				{
+					try 
+					{
+						client.delGroups(Group).catch(()=>{})
+					}
+					catch (e) {console.log(e)}
+				}
+			} 
+			catch {}
+			break
+
+
+
+
+		case "moveclienttochannel":
+			try 
+			{
+				client = await teamspeak.getClientByName(Args[0]);
+				const Channels = await teamspeak.channelList()
+				var channel = null
+				Channels.forEach(c => {
+					if (c.name==Args[1])
+					{
+						channel = c
+						
+					}
+
+				})
+
+				if (channel != null)
+				{
+					try 
+					{
+						client.move(channel).catch(()=>{})
+					}
+					catch (e) {console.log(e)}
+				}
+			}
+			catch {}	
+			break	 
+		
+
+
+		case  "kickclientfromchannel":
+			client = await teamspeak.getClientByName(Args[0]);
+			try 
+			{
+				client.kickFromChannel().catch(()=>{})
+			}
+			catch {}
+			break
+
+	
+
+
+
+		case  "kickclientfromserver":
+			client = await teamspeak.getClientByName(Args[0]);
+			try 
+			{
+				client.kickFromServer().catch(()=>{})
+			}
+			catch {}
+			break
+
+
+		case  "banclient":	
+			try 
+			{	
+				client = await teamspeak.getClientByName(Args[0]);
+				client.ban(Args[1], Args[2]).catch(()=>{})
+				break
+			} catch {}
+	
+
+
+	}
+
+	
+
+
+}
--- website/teamspeak/scripts/teamspeak_parser.js
+++ website/teamspeak/scripts/teamspeak_parser.js
@@ -2,10 +2,18 @@
 
 var CommandElement = document.getElementById("commands");
 
+var EventElement = document.getElementById("events");
 
-const CommandEntryPrefab ='<div class="Command">                                      \
+
+
+
+
+
+
+
+const CommandEntryPrefab ='<div class="Command">                                     \
                     <label for="Command[ID]"> When a User sends the Command: </label> \
-                    <input type="text" placeholder="CommandName" id="Command[ID]">    \
+                    <input type="text" placeholder="CommandName" id="Command[ID]">    <button class="SubButton" onclick="RemCommandEntry([ID])">X</button>\
                     <br>                                                              \
                                                                                       \
                     <div class="Action">                                              \
@@ -14,28 +22,55 @@ const CommandEntryPrefab ='<div class="Command">                                
                           [ACTIONS]                                                   \
                         </select><br>                                                 \
                         <div id="CommandActionArgs[ID]">  </div>                         \
-                  </div><button class="AddButton" onclick="RemCommandEntry(CommandAction[ID])">-</button>                                                          \
+                  </div>                                                          \
                   </div> \
+';
+
+var EventEntryPrefab   ='<div class="Command">    \
+<label for="Event[ID]"> When: </label>            \
+<select id="EventSelect[ID]">                           \
+  [EVENTS]                                        \
+</select>                                         <button class="SubButton" onclick="RemEventEntry([ID])">X</button>\
+<div id="EventOption[ID]">                        \
+</div>                                            \
+<div class="Action">                              \
+    <label for="ActionEventArg[ID]"> Then </label>   \
+    <select id="EventAction[ID]">                 \
+      [ACTIONS]                                   \
+    </select><br>                                 \
+    <div id="EventActionArgs[ID]">  </div>        \
+</div> \
+</div> \
 ';
 
 
 
-CommandID = 0;
 
+
+
+
+
+
+
+
+CommandID = 0;
+EventID = 0;
 
 
 
 const ActionOptionPrefab = ' <option value="[OPT]">[OPT]</option> '
 
 
-
-const ActionDisplayInputPrefab = ' <input id="ActionArg[ACTION]|[ID]"> '
-
+const ActionDisplayInputPrefab = ' <input id="ActionArg[TYPE]|[ACTION]|[ID]"> '
 
 
-const ExistingCommands = []
+
+const EventArgPrefab = ' <input id="EventArg[ACTION]"> '
 
 
+const ExistingCommands = [];
+
+const ExitstingEvents = [];
 
 
 
@@ -69,14 +104,13 @@ function UpdateCommand(El)
   var selectedOption = El.options[El.selectedIndex].value;
   ID = El.id.toString().replace(/\D/g,'');
   const ArgsObj = document.getElementById("CommandActionArgs"+ID)
-  ArgsObj.innerHTML = ActionDescMap[selectedOption].replace(/\[ACTION\]/,ID.toString())
+  ArgsObj.innerHTML = ActionDescMap[selectedOption].replace(/\[ACTION\]/g,ID.toString()).replace(/\[TYPE\]/g,"1")
 }
 
 
 
-function RemCommandEntry(El)
+function RemCommandEntry(ID)
 {
-  ID = El.id.toString().replace(/\D/g,'');
   const ArgsObj = document.getElementById("Command"+ID).parentElement;
   CommandElement.removeChild(ArgsObj)
   const index = ExistingCommands.indexOf(ID);
@@ -84,6 +118,78 @@ function RemCommandEntry(El)
     ExistingCommands.splice(index, 1);
   }
 }
+
+
+
+
+
+
+
+
+
+var EventPerms = {}
+
+
+
+function AddEventEntry()
+{
+
+  if (EventElement == null)
+  {
+    EventElement = document.getElementById("events");
+  }
+  var Command = EventEntryPrefab;
+  Command = Command.replace(/\[ID\]/g, EventID.toString());
+  Command = Command.replace(/\[ACTIONS\]/g, ActionOptions.join(""));
+
+
+
+  EventElement.appendChild(createElementFromHTML(Command));
+  document.getElementById("EventAction"+EventID.toString()).addEventListener('change', function (){UpdateEvent(this)}, false);
+  UpdateEvent(document.getElementById("EventAction"+EventID.toString()) )
+  ExitstingEvents.push(EventID)
+  document.getElementById("EventSelect"+EventID.toString()).addEventListener('change', function (){UpdateEventPerm(this)}, false);
+  UpdateEventPerm(document.getElementById("EventSelect"+EventID.toString()))
+  EventID++;
+
+}
+
+
+function UpdateEventPerm(El)
+{
+  var selectedOption = El.options[El.selectedIndex].value;
+  ID = El.id.toString().replace(/\D/g,'');
+  const ArgsObj = document.getElementById("EventOption"+ID)
+  ArgsObj.innerHTML = EventPerms[selectedOption].replace(/\[ACTION\]/g,ID.toString())
+}
+
+
+function UpdateEvent(El)
+{
+  var selectedOption = El.options[El.selectedIndex].value;
+  ID = El.id.toString().replace(/\D/g,'');
+  const ArgsObj = document.getElementById("EventActionArgs"+ID)
+  ArgsObj.innerHTML = ActionDescMap[selectedOption].replace(/\[ACTION\]/g,ID.toString()).replace(/\[TYPE\]/g,"2")
+}
+
+
+
+function RemEventEntry(ID)
+{
+  const ArgsObj = document.getElementById("EventSelect"+ID).parentElement;
+  EventElement.removeChild(ArgsObj)
+  const index = ExistingCommands.indexOf(ID);
+  if (index > -1) {
+    ExitstingEvents.splice(index, 1);
+  }
+}
+
+
+
+
+
+
+
 
 
 
@@ -107,6 +213,8 @@ function defineAll()
   Config = JSON.parse(loadData("./src/node/config.json"));
   ActionOptions = []
   ActionDescMap = {};
+  ArgNumMap = {}
+  EventArgNumMap = {}
 
   HelpData.Actions.forEach(Action=>{
     	ActionOptions.push(ActionOptionPrefab.replace(/\[OPT\]/g, Action.name))
@@ -115,15 +223,78 @@ function defineAll()
       {
         ActionDesc = ActionDesc.replace(new RegExp("<"+i+">") , ActionDisplayInputPrefab.replace(/\[ID\]/, i.toString()))
       }
-
+      ArgNumMap[Action.name] = Action.ArgNum
       ActionDescMap[Action.name] = ActionDesc
   })
+
+
+
+  HelpData.Events.forEach(Action=>{
+    var ActionDesc = Action.display;
+    
+    if (Action.ArgNum==1)
+    {
+      ActionDesc = ActionDesc.replace(/<>/g , EventArgPrefab)
+    }
+
+
+    ArgNumMap[Action.name] = Action.ArgNum
+   
+    EventPerms[Action.name] = ActionDesc
+})
+ EventEntryPrefab = EventEntryPrefab.replace(/\[EVENTS\]/g, Object.keys(EventPerms).map(key=>{return ActionOptionPrefab.replace(/\[OPT\]/g, key)}).join(""))
+
 
 }
 
 
 
 
+
+function MakeDownloadable ()
+{
+  Config.host = document.getElementById("Host").value;
+  Config.login = document.getElementById("ServerQueryName").value;  
+  Config.password = document.getElementById("ServerQueryPass").value;  
+  Config.port = document.getElementById("Port").value;  
+  Config.name = document.getElementById("Nickname").value;   
+
+  ExistingCommands.forEach(Command=>{
+    const CommandName = document.getElementById("Command"+Command).value
+    const El = document.getElementById("CommandAction"+Command)
+    const selectedOption = El.options[El.selectedIndex].value;
+    var ActionArgs = []
+    for (var i = 1; i != ArgNumMap[selectedOption] + 1; i++)
+    {
+      ActionArgs.push(document.getElementById("ActionArg1|"+Command+"|"+i).value)
+    }
+    Config.commands.push({"name": CommandName, "argNum": 0, "action": {"name": selectedOption, "args":ActionArgs}})
+  })
+
+
+
+  ExitstingEvents.forEach(Command=>{
+    const E2 = document.getElementById("EventSelect"+Command)
+    const selectedOptionEvent = E2.options[E2.selectedIndex].value;
+
+    const El = document.getElementById("EventAction"+Command)
+    const selectedOption = El.options[El.selectedIndex].value;
+    var ActionArgs = []
+    Condition = ""
+    if (1 == EventPerms[selectedOptionEvent])
+    {
+      Condition = document.getElementById("ActionEventArg"+Command).value
+    }
+    for (var i = 1; i != ArgNumMap[selectedOption] + 1; i++)
+    {
+      ActionArgs.push(document.getElementById("ActionArg2|"+Command+"|"+i).value)
+    }
+    Config.events.push({"name": selectedOptionEvent, "condition": Condition, "action": {"name": selectedOption, "args":ActionArgs}})
+  })
+
+
+  download ("config.json", JSON.stringify(Config))
+}
 
 
 
@@ -133,12 +304,8 @@ function defineAll()
 // args = Argument -> in der Array Form: ["Argument=Wert", ""]
 function loadData(doc, args=[])
 {
-	OArgs = "";
-	for (i=0;i!=args.length;i++)
-	{
-		OArgs += args[i] + "&";
-	}
-	OArgs = OArgs.substring(0, OArgs.length - 1);
+	OArgs = doc + args.join("&");
+
 	var request = new XMLHttpRequest();
     request.open('POST', doc , false);
 	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -148,4 +315,20 @@ function loadData(doc, args=[])
 
 
 
+function download(filename, text) 
+{
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+
 defineAll()
+
